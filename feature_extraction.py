4b65d40f7bdb17a994216ef0ff374ac48eb2fdf0
George Oblapenko <kunstmord@kunstmord.com>
2018-08-04 17:59:45 +0300

    Re-wrote feature extraction to ease use in generation code

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,14 +1,17 @@
 import numpy as np
 
 
-def extract_features(spectrogram, config, offset=0):
-    # print('Extracting features, ')
-    # output = np.zeros((spectrogram.shape[0], config['n_train']))
-    output = spectrogram[offset:offset + config['n_train'] * config['step']:config['step'], :]
-    return output
+def extract_features_single_frame(spectrogram, n_frame, config):
+    return spectrogram[n_frame-1, :]
 
 
 def extract_target(spectrogram, config):
-    # output = np.zeros((spectrogram.shape[0], config['n_train']))
-    output = spectrogram[1:1 + config['n_train'] * config['step']:config['step'], :]
+    output = spectrogram[config['start_offset']:config['start_offset'] + config['n_train'] * config['step']:config['step'], :]
+    return output
+
+
+def extract_features(spectrogram, config):
+    output = np.zeros((config['n_train'], spectrogram.shape[1]))
+    for i in range(config['n_train']):
+        output[i, :] = extract_features_single_frame(spectrogram, config['start_offset'] + i * config['step'], config)
     return output

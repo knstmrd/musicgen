08df213c76e3b081f114c06210a0a5d7b1b494ba
George Oblapenko <kunstmord@kunstmord.com>
2018-08-08 12:50:38 +0300

    limits on min/max db added

--- generation.py
+++ generation.py
@@ -16,8 +16,12 @@ def generate(spectrogram, predictor, config, n_features):
     print(y.shape, spectrogram.shape)
     y[:config['seed_length'], :] = spectrogram[config['test_offset']:config['test_offset'] + config['seed_length'], :]
     for i in range(config['n_test']):
+        # print(i)
         if i % config['gen_verbose'] == 0:
             print('Generating frame {}/{}'.format(i, config['n_test']))
         features = fp.extract_features_single_frame(y, config['seed_length'] + i, config).reshape(1, -1)
-        y[config['seed_length'] + i, :] = generate_frame_single(features, predictor)
+        frame_single = generate_frame_single(features, predictor)
+        frame_single[frame_single >= 0] = config['db_max']
+        frame_single[frame_single <= -120] = -120
+        y[config['seed_length'] + i, :] = frame_single
     return y
